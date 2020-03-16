a6243ebe7e655aaae2b4d6da68bdf0c583058da5
Rajeev <rajeevchaudhary@inbox.ru>
2020-03-16 15:48:16 +0530

    updated

--- js/home.js
+++ js/home.js
@@ -1,7 +1,8 @@
+
 function openNav() {
-    document.getElementById("myNav").style.width = "50%";
+    document.getElementById("myNav").style.width = "70%";
   }
   
   function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myNav").style.width = "0%";
   }
